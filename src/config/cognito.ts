9d21a7dd97bd9f9da0bf0f449ed94ccabc029d4a
John Caskey <110431166+john-caskey@users.noreply.github.com>
2025-12-19 10:34:48 -0500

    Update cognito.ts

--- src/config/cognito.ts
+++ src/config/cognito.ts
@@ -25,10 +25,10 @@ export interface CognitoConfig {
  */
 export const cognitoConfig: CognitoConfig = {
   // AWS Cognito User Pool ID (e.g., 'us-east-1_xxxxxxxxx')
-  userPoolId: process.env.COGNITO_USER_POOL_ID || '',
+  userPoolId: process.env.COGNITO_USER_POOL_ID || 'us-east-2_Z6wNcT7sN',
   
   // AWS Cognito App Client ID (e.g., '1234567890abcdefghijklmnop')
-  clientId: process.env.COGNITO_CLIENT_ID || '',
+  clientId: process.env.COGNITO_CLIENT_ID || '40b923fpk6c5v1lvatbcqbdakq',
   
   // AWS Region where your Cognito User Pool is located (e.g., 'us-east-1', 'us-west-2')
   region: process.env.AWS_REGION || 'us-east-1',
